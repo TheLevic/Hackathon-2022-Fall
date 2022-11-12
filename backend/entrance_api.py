4256df51fe3365bd9ff2d686a2afc1c50d443da9
unknown <bzheng99.real@gmail.com>
2022-11-12 00:18:28 -0600

    backend

--- backend/entrance_api.py
+++ backend/entrance_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 import db
 
@@ -7,7 +7,12 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app, resources={r"./*":{'origins': '*'}})
 
+@app.route("/handle", methods=["POST"])
+def handle():
+    print(request.json)
+    return Response("", status=200)
+
 if __name__ == "__main__":
-    test = db.DB()
-    test.createDatabase()
-    app.run()
+    # test = db.DB()
+    # test.createDatabase()
+    app.run(host="0.0.0.0")
