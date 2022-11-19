d7b2ec051f3a15dbcf8b939ce1f1b3c49fb47b20
TheLevic <lccrider@uark.edu>
2022-11-19 11:12:51 -0600

    Data is uplaoded and stored properly.

--- backend/entrance_api.py
+++ backend/entrance_api.py
@@ -8,7 +8,7 @@ Debug = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 CORS(app, resources={r"./*":{'origins': '*'}})
-name = ["Test"];
+name = ["Test", "Test2"];
 
 def createDirectory(name):
     try:
@@ -19,32 +19,6 @@ def createDirectory(name):
     except:
         print("Error creating directory");
 
-
-@app.route("/handle", methods=["GET","POST"])
-def handle():
-    if request.method == "POST":
-        print(len(request.files.getlist('files')));
-        # Get the names 
-        songName = request.form['song']
-        userName = request.form['name']
-        print(songName, userName);
-        createDirectory(userName);
-
-        # Getting the filess
-        if (len(request.files.getlist('files')) > 1):
-            post_data = request.files.getlist('files');
-            for file in post_data:
-                file.save(f'../Images/{userName}/{file.filename}');
-        elif (len(request.files.getlist('files')) == 1):
-            file = request.files.getlist('files')[0];
-            file.save(f'../Images/{userName}/{file.filename}');
-        personsFilePath = "../Images/" + userName +"/";
-        #Get the song
-        database = db.DB();
-        database.createDatabase();
-#        database.insertName(userName,personsFilePath,songName);
-    return Response("", status=200);
-
 #Method to get our users name from the front end, make a directory, and then store it in our database
 @app.route("/name", methods=["POST"])
 def getName():
@@ -52,6 +26,9 @@ def getName():
     if content_type == "application/json":
         data = request.json;
         name[0] = data['Name'];
+        database = db.DB();
+        database.createDatabase();
+        database.insertName(name[0]);
         createDirectory(str(data['Name']));
     return Response("", status=200);
 
@@ -63,5 +40,15 @@ def getUserData():
         image.save(f'../Images/{name[0]}/{filename}');
     return Response("", status=200);
     
+@app.route("/songName", methods=["POST"])
+def getSongName():
+    contentType = request.headers['Content-Type'];
+    if contentType == "application/json":
+        data = request.json;
+        name[1] = data['Song']; 
+        database = db.DB();
+        database.createDatabase();
+        database.updateSongName(name[0], name[1]);
+        return Response("Got song", status=200);
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=3000);
